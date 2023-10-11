2cc6ae28e02b08f96cfed3e61e27b9dc0c78ee12
oussemabh99 <oussema.benhdid@etudiant-enit.utm.tn>
2023-10-11 21:36:52 +0100

    3 eme commit

--- script.php
+++ script.php
@@ -5,9 +5,27 @@ $password = 'oussemaest19';
 $conn = new mysqli($servername, $username, $password);
 if($conn->connect_error){
 die('Erreur : ' .$conn->connect_error);}
-echo 'Connexion réussie';
-$result = $conn->query("SELECT * FROM projet.session ");
-foreach ($result as $row) {
-  echo "  Name = " . $row['Nom'] . "\n";
+else 
+{
+  $result = $conn->query("SELECT * FROM projet.session ");
+  foreach ($result as $row) {
+    if ($row['Nom']==$_POST['username'])
+      {
+        if($row["Password"]==$_POST['password'])
+         {
+          session_start();
+          $_SESSION['Nom']=$_POST["Username"] ; 
+          header('Location: http://www.facebook.com');
+          exit();
+         
+         }
+        else
+        {
+          echo 'mot de pass incorrect'; 
+        }
+      }
+       
+  }
 }
+
 ?>
